a00c362776efd2f7efdfcdf8e4cb953b559710d1
Danny Paez <dpaez16@yahoo.com>
2020-08-21 09:37:38 -0700

    p107 done

--- p107.cpp
+++ p107.cpp
@@ -22,16 +22,17 @@ void process_line(string & line, int r, unordered_map<pair<int, int>, unsigned, 
 	int c = 0;
 	
 	while(getline(ss, temp_str, ',')) {
-		if (temp_str == "-") goto END_LOOP;
-
-		unsigned elem = stoi(temp_str);
-		neighbors.push_back(elem);
-
-		END_LOOP:
+		if (temp_str == "-"  || c <= r) {
 			c++;
-	}
+			continue;
+		};
 
-	return neighbors;
+		unsigned edge_weight = stoi(temp_str);
+		edge_weights[PAIR(r, c)] = edge_weight;
+		edge_weights[PAIR(c, r)] = edge_weight;
+
+		c++;
+	}
 }
 
 unordered_map<pair<int, int>, unsigned, pair_hash> process_file() {
@@ -40,18 +41,83 @@ unordered_map<pair<int, int>, unsigned, pair_hash> process_file() {
 	file.open(FILE_PATH);
 
 	unordered_map<pair<int, int>, unsigned, pair_hash> edge_weights;
+	int r = 0;
 
 	while (getline(file, line)) {
-		process_line(line, edge_weights);
-		edges.push_back(neighbors);
+		process_line(line, r, edge_weights);
+		r++;
 	}
 
 	file.close();
-	return matrix;
+	return edge_weights;
+}
+
+unordered_set<int> get_vertices() {
+	unordered_set<int> vertices;
+
+	for (int i = 1; i < N; i++) {
+		vertices.insert(i);
+	}
+
+	return vertices;
+}
+
+unsigned get_graph_weight(unordered_map<pair<int, int>, unsigned, pair_hash> & edge_weights) {
+	unsigned graph_weight = 0;
+
+	for (int i = 0; i < N; i++) {
+		for (int j = i + 1; j < N; j++) {
+			if (edge_weights.find(PAIR(i, j)) == edge_weights.end()) continue;
+
+			graph_weight += edge_weights[PAIR(i, j)];
+		}
+	}
+
+	return graph_weight;
+}
+
+pair<int, unsigned> get_next_vertex_and_weight(unordered_map<pair<int, int>, unsigned, pair_hash> & edge_weights, vector<int> & mst_vertices, unordered_set<int> & vertices) {
+	unsigned min_edge_weight = INFTY;
+	int optimal_adj_vertex = -1;	
+
+	for (int vertex_a : mst_vertices) {
+		for (int vertex_b : vertices) {
+			if (edge_weights.find(PAIR(vertex_a, vertex_b)) == edge_weights.end()) continue;
+
+			unsigned edge_weight = edge_weights[PAIR(vertex_a, vertex_b)];
+
+			if (min_edge_weight > edge_weight) {
+				min_edge_weight = edge_weight;
+				optimal_adj_vertex = vertex_b;
+			}
+		}
+	}
+
+	pair<int, unsigned> next_vertex_and_weight(optimal_adj_vertex, min_edge_weight);
+	return next_vertex_and_weight;
 }
 
 
 int main() {
-	vector<vector<unsigned>> matrix = process_file();
+	unordered_map<pair<int, int>, unsigned, pair_hash> edge_weights = process_file();
+	vector<int> mst_vertices = {0};
+	unordered_set<int> vertices = get_vertices();
+	unsigned mst_total = 0;
+
+	while (mst_vertices.size() != N) {
+		pair<int, unsigned> next_step = get_next_vertex_and_weight(edge_weights, mst_vertices, vertices);
+		int optimal_adj_vertex = next_step.first;
+		unsigned min_edge_weight = next_step.second;
+
+		mst_vertices.push_back(optimal_adj_vertex);
+		vertices.erase(optimal_adj_vertex);
+
+		mst_total += min_edge_weight;
+	}
+
+	unsigned total_graph_weight = get_graph_weight(edge_weights);
+	unsigned weight_saved = total_graph_weight - mst_total;
+
+	cout << weight_saved << endl;
 	return 0;
 }
