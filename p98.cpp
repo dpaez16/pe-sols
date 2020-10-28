4b71127f7128ad35de47a8136b1104a8379c9ba1
Danny Paez <dpaez16@yahoo.com>
2020-10-28 16:32:14 -0700

    completed anagram functions, added is_square

--- p98.cpp
+++ p98.cpp
@@ -4,16 +4,15 @@ using namespace std;
 
 #define FILE_PATH "files/p098_words.txt"
 
-set<string> process_file() {
+unordered_set<string> process_file() {
 	string line;
 	ifstream file;
 	file.open(FILE_PATH);
-	set<string> words;
+	unordered_set<string> words;
 
 	while(getline(file, line)) {
 		stringstream ss(line);
 		string raw_word;
-		vector<int> row;
 		
 		while(getline(ss, raw_word, ',')) {
 			size_t word_size = raw_word.size() - 2;
@@ -26,8 +25,55 @@ set<string> process_file() {
 	return words;
 }
 
+bool is_anagram(string word, string other_word) {
+	sort(word.begin(), word.end());
+	sort(other_word.begin(), other_word.end());
+
+	return word == other_word;
+}
+
+unordered_set<string> get_anagrams(string & word, unordered_set<string> & words) {
+	unordered_set<string> anagrams;
+
+	for (string possible_anagram : words) {
+		if (!is_anagram(word, possible_anagram))
+			continue;
+
+		anagrams.insert(possible_anagram);
+	}
+
+	for (string anagram : anagrams) {
+		words.erase(anagram);
+	}
+
+	return anagrams;
+}
+
+vector<unordered_set<string>> get_anagram_groups(unordered_set<string> & words) {
+	vector<unordered_set<string>> anagram_groups;
+
+	while (!words.empty()) {
+		string word = *(words.begin());
+		unordered_set<string> anagrams = get_anagrams(word, words);
+		
+		if (anagrams.size() == 1)
+			continue;
+
+		anagram_groups.push_back(anagrams);
+	}
+
+	return anagram_groups;
+}
+
+bool is_square(int n) {
+	int sqrt_n = floor(sqrt(n));
+
+	return pow(sqrt_n, 2) == n;
+}
+
 int main() {
-	set<string> words = process_file();
+	unordered_set<string> words = process_file();
+	vector<unordered_set<string>> anagram_groups = get_anagram_groups(words);
 
 	return 0;
 }
