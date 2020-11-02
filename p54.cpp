4f529c6258414c936ff3a65e0323bbe1dd3fea06
Danny Paez <dpaez16@yahoo.com>
2020-11-02 12:36:04 -0800

    fixed bug with file processing

--- p54.cpp
+++ p54.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 #define FILE_PATH "files/p054_poker.txt"
+#define HAND_SIZE 5
 
 typedef struct {
     char suit;
@@ -10,7 +11,7 @@ typedef struct {
 } card;
 
 typedef struct {
-    card cards[5];
+    card cards[HAND_SIZE];
 } hand;
 
 enum Rank {    
@@ -26,12 +27,27 @@ enum Rank {
     royal_flush
 };
 
-unordered_map<char, int> VAL_MAP = {
-    {'J', 10},
-    {'Q', 11},
-    {'K', 12},
-    {'A', 13}
+unordered_map<string, int> VAL_MAP = {
+    {"T", 10},
+    {"J", 11},
+    {"Q", 12},
+    {"K", 13},
+    {"A", 14}
 };
+
+card get_card(string & raw_card) {
+    int size = raw_card.size();
+    card c;
+    
+    char suit = raw_card[size - 1];
+    string value = raw_card.substr(0, size - 1);
+    int value_int = all_of(value.begin(), value.end(), ::isdigit) ? stoi(value) : VAL_MAP[value];
+
+    c.suit = suit;
+    c.value = value_int;
+
+    return c;
+}
 
 pair<hand, hand> process_line(string & line) {
     hand p1, p2;
@@ -40,25 +56,14 @@ pair<hand, hand> process_line(string & line) {
     string raw_card;
     
 
-    for (unsigned idx = 0; idx < 5; idx++) {
+    for (unsigned idx = 0; idx < HAND_SIZE; idx++) {
         getline(ss, raw_card, ' ');
-
-        char value = raw_card[0];
-        int value_int = isdigit(value) ? value - '0' : VAL_MAP[value];
-        char suit = raw_card[1];
-
-        p1.cards[idx].value = value_int;
-        p1.cards[idx].suit = suit;
+        p1.cards[idx] = get_card(raw_card);
     }
 
-    for (unsigned idx = 0; idx < 5; idx++) {
+    for (unsigned idx = 0; idx < HAND_SIZE; idx++) {
         getline(ss, raw_card, ' ');
-
-        char value = raw_card[0];
-        char suit = raw_card[1];
-
-        p2.cards[idx].value = value;
-        p2.cards[idx].suit = suit;
+        p2.cards[idx] = get_card(raw_card);
     }
 
     pair<hand, hand> game(p1, p2);
@@ -88,9 +93,19 @@ bool card_lt(const card & left, const card & right) {
     return left.value < right.value;
 }
 
+void print_cards(const card cards[HAND_SIZE]) {
+    for (unsigned idx = 0; idx < HAND_SIZE; idx++) {
+        cout << cards[idx].value << cards[idx].suit << ' ';
+    }
+
+    cout << endl;
+}
+
 Rank get_rank(hand & player) {
     // TODO
-    sort(player.cards, player.cards + 5, card_lt);
+    print_cards(player.cards);
+    sort(player.cards, player.cards + HAND_SIZE, card_lt);
+    print_cards(player.cards);
     return royal_flush;
 }
 
@@ -115,6 +130,7 @@ int main() {
         hand p2 = game.second;
 
         if (p1_winner(p1, p2)) count++;
+        break;
     }
 
     cout << count << endl;
