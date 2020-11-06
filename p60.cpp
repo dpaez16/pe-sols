c1684018ce6862eaa8b4ed3fa4019aa1ed36c17d
Danny Paez <dpaez16@yahoo.com>
2020-11-06 12:22:29 -0800

    refactoring

--- p60.cpp
+++ p60.cpp
@@ -22,6 +22,43 @@ bool is_prime(int n) {
 	return true;
 }
 
+void update_subgraph(int left_int, int right_int, edge_map & edges, graph & subgraph) {
+    auto left_incoming_vertices = edges[right_int];
+    
+    if (left_incoming_vertices.find(left_int) == left_incoming_vertices.end()) {
+        return;
+    }
+
+    subgraph.vertices.insert(left_int);
+    subgraph.vertices.insert(right_int);
+    
+    int a = min(left_int, right_int);
+    int b = max(left_int, right_int);
+    subgraph.edges[a].insert(b);
+}
+
+void iterate_through_splits(int p, unordered_set<int> & primes, edge_map & edges, graph & subgraph) {
+    string p_str = to_string(p);
+
+    for (unsigned idx = 0; idx + 1 < p_str.size(); idx++) {
+        string left = p_str.substr(0, idx + 1);
+        string right = p_str.substr(idx + 1);
+
+        if (right[0] == '0') continue;
+
+        int left_int = stoi(left);
+        int right_int = stoi(right);
+
+        if (primes.find(left_int) == primes.end()) continue;
+        if (primes.find(right_int) == primes.end()) continue;
+
+        // a -> b <==> ab forms prime
+        edges[left_int].insert(right_int);
+
+        update_subgraph(left_int, right_int, edges, subgraph);
+    }
+}
+
 int main() {
     unordered_set<int> primes;
     edge_map edges;
@@ -38,32 +75,7 @@ int main() {
 
         primes.insert(p);
         edges[p] = set<int>();
-        string p_str = to_string(p);
-
-        for (unsigned idx = 0; idx + 1 < p_str.size(); idx++) {
-            string left = p_str.substr(0, idx + 1);
-            string right = p_str.substr(idx + 1);
-
-            if (right[0] == '0') continue;
-
-            int left_int = stoi(left);
-            int right_int = stoi(right);
-
-            if (primes.find(left_int) == primes.end()) continue;
-            if (primes.find(right_int) == primes.end()) continue;
-
-            // a -> b <==> ab forms prime
-            edges[left_int].insert(right_int);
-
-            auto left_incoming_vertices = edges[right_int];
-            if (left_incoming_vertices.find(left_int) != left_incoming_vertices.end()) {
-                subgraph.vertices.insert(left_int);
-                subgraph.vertices.insert(right_int);
-                
-                subgraph.edges[left_int].insert(right_int);
-                subgraph.edges[right_int].insert(left_int);
-            }
-        }
+        iterate_through_splits(p, primes, edges, subgraph);
 
         p++;
     }
