ac555300ccad2e9ecd7309f6b923f65cea2d8f65
Danny Paez <dpaez16@yahoo.com>
2020-12-07 08:28:41 -0800

    p120 much faster

--- p120.py
+++ p120.py
@@ -4,14 +4,12 @@ A_MAX = 10 ** 3
 N = 10 ** 4
 
 def get_r(a):
-    r_max = 0
     a_sq = a ** 2
+    r_max = 2 % a_sq
 
-    for n in range(N + 1):
-        b1 = pow(a - 1, n, a_sq)
-        b2 = pow(a + 1, n, a_sq)
-        r = pow(b1 + b2, 1, a_sq)
-
+    for n in range(1, N + 1, 2):
+        num = 2 * n * a
+        r = num % a_sq
         r_max = max(r_max, r)
 
     return r_max
