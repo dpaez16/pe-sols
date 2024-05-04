c9380403f0170f448807cbcfd8cb8166bcfee37c
Danny <dpaez16@yahoo.com>
2024-05-03 18:01:07 -0700

    corrected the way bit_length is calculated to a more portable method

--- p810.cpp
+++ p810.cpp
@@ -2,11 +2,22 @@
 
 using namespace std;
 
-#define NUM_BITS(x) (8 * sizeof(x))
-#define BIT_LENGTH(x) (NUM_BITS(x) - __builtin_clz(x))
-
 const int N = 5 * pow(10, 6);
 const int M = 27; // According to https://oeis.org/A173270/list, the 5 millionth xor prime occurs in degree 26, so 2^27 is our upper bound
+
+int bit_length(long x) {
+    if (x < 0) {
+        x *= -1;
+    }
+
+    int bit_count = 0;
+    while (x > 0) {
+        x >>= 1;
+        bit_count++;
+    }
+
+    return bit_count;
+}
 
 int main() {
     unsigned int size = pow(2, M);
@@ -21,7 +32,7 @@ int main() {
         if (!is_prime[p]) continue;
 
         long x = p;
-        const int m = M - BIT_LENGTH(p);
+        const int m = M - bit_length(p);
         const int inner_upper = 1 << (m + 1);
 
         for (int k = 1; k < inner_upper; k++) {
